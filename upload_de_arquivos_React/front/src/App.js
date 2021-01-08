2753a2dbc0aa2555b0409a0c4ca3693b547c1404
Monitor Laboratório 1 <monitorlab1@unifacef.edu.br>
2021-01-08 19:36:53 -0300

    front quase finalizado

---
+++ upload_de_arquivos_React/front/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import GlobalStyle from './styles/global'
+
+import { Container, Content} from './styles'
+
+import Upload from './components/Upload/Upload'
+import FileList from './components/FileList'
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Upload/>
+          <FileList/>
+        </Content>
+        <GlobalStyle/>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ upload_de_arquivos_React/front/src/styles/global.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+import 'react-circular-progressbar/dist/styles.css'
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+       font-family: Arial, Helvetica, sans-serif;
+       font-size: 14px;
+       background: #7159c1;
+       text-rendering: optimizeLegibility;
+       -webkit-font-smoothing: antialiased;
+    }
+
+   body, html, #root{
+        height: 100%;
+
+    }
+
+`
