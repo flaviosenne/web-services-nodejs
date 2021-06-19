d83e9cbe48deb3a5c8a181cfa2fc00702b82b421
Joao Flavio <flaviosenne123@gmail.com>
2021-06-18 21:52:04 -0300

    confirm libre json2xls convert array

--- generate_excel/index.js
+++ generate_excel/index.js
@@ -1,12 +1,18 @@
-var json2xls = require('json2xls');
-var fs = require('fs')
-var json = {
+const json2xls = require('json2xls');
+const fs = require('fs')
+const json = [{
     foo: 'bar',
     qux: 'moo',
     poo: 123,
     date: new Date().toDateString('dd/MM/yyyy')
-}
+},
+{
+    foo: 'bar 2',
+    qux: 'moo 2',
+    poo: 1234,
+    date: new Date('2021-05-20').toDateString()
+}]
 
-var xls = json2xls(json);
+const xls = json2xls(json);
 
 fs.writeFileSync('data.xlsx', xls, 'binary');
