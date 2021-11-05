35a31c737be218f08eb0c199f438dc32f3ee648b
joao flavio <flaviosenne123@gmail.com>
2021-11-05 10:36:27 -0300

    connect with sqlite successfuly

--- project-nest/src/app.module.ts
+++ project-nest/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { join } from 'path'
 
+console.log('aqui  ', __dirname)
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db',
-      entities: ['./entities/*.entity.ts'],
+      database: 'src/database/db.sqlite',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
    })
     ,UsersModule],
